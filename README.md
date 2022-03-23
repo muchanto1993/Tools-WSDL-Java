ce5542aed7b6b0216925d850ac02760191f71345
Anto <m.antoberlianto1993@gmail.com>
2022-03-23 16:23:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tools-WSDL-Java
+ Tools for Generate Java Class from WSDL
---
+++ src/main/java/com/mab/wsdl/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.localhost.com/springsoap/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.mab.wsdl;
